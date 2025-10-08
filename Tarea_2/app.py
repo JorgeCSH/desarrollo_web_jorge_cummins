111f7a2931f0959923a7e192bc935b2f40035e8c
JorgeCSH <jorge.cumminz@gmail.com>
2025-10-08 15:10:35 -0300

    feat: new templates (read comments) add new version of the templates and starting all over again

--- Tarea_2/app.py
+++ Tarea_2/app.py
@@ -1,31 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for, session
-#from database import db
+from utils.validations import *
+from database import db
+from werkzeug.utils import secure_filename
+import hashlib
 import filetype
 import os
 
 UPLOAD_FOLDER = 'static/uploads'
 
 app = Flask(__name__)
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    mascotas = obtener_ultimas_5()
-    return render_template('index.html', mascotas=mascotas)
+    return render_template('index.html', active_page='index')
 
-@app.route('/listado')
+@app.route('/adoption-list')
 def adoption_list():
-    avisos = obtener_todos_avisos()
-    return render_template('adoption-list.html', avisos=avisos)
+    return render_template('adoption-list.html', active_page='adoption_list')
 
-@app.route('/publicar', methods=['GET', 'POST'])
+@app.route('/add-adoption')
 def add_adoption():
-    if request.method == 'POST':
-        # Procesar formulario
-        pass
-    return render_template('add-adoption.html', regiones=REGIONES)
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template('add-adoption.html', active_page='add_adoption')
+
+@app.route('/adoption-stats')
+def adoption_stats():
+    return render_template('adoption-stats.html', active_page='adoption_stats')
